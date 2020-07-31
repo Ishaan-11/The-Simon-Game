35b005dc549af8acfaf3120419d169a70609f570
Ishaan-11 <ishaansharma1998@gmail.com>
2020-07-31 21:23:11 +0530

    12

--- game.js
+++ game.js
@@ -8,16 +8,16 @@ var userClickedPattern = [];
 $(".btn").on("click", function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    // play the sound for the button colour selected
+    playSound(userChosenColour);
 });
 
 //generate a new random number between 0 and 3,
 function nextSequence() {
     
     var randomNumber = Math.floor(Math.random() * 4);
-    
     //to select a random colour from the buttonColours array.
     var randomChosenColour = buttonColours[randomNumber];
-
     // Add the new randomChosenColour to the end of the gamePattern.
     gamePattern.push(randomChosenColour);
 
@@ -25,6 +25,12 @@ function nextSequence() {
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     // play the sound for the button colour selected
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+
+}
+
+// play the sound for the button colour selected
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
