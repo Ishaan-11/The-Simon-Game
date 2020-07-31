5021d41570a8c2cf9517dffd453cbd992d2d245f
Ishaan-11 <ishaansharma1998@gmail.com>
2020-07-31 22:18:28 +0530

    18

--- game.js
+++ game.js
@@ -1,23 +1,11 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
 //to keep track of whether if the game has started or not
 var started = false;
-
 var level = 0;
-
-//detect when any of the buttons are clicked
-$(".btn").on("click", function() {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    // play the sound for the button colour selected
-    playSound(userChosenColour);
-    //animate button on click
-    animatePress(userChosenColour)
-});
 
 // when a keyboard key has been pressed call nextSequence().
 $(document).on("keydown",function() {
@@ -29,8 +17,39 @@ $(document).on("keydown",function() {
     }
 });
 
+//detect when any of the buttons are clicked
+$(".btn").on("click", function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+    // play the sound for the button colour selected
+    playSound(userChosenColour);
+    //animate button on click
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+        console.log('success');
+        if(userClickedPattern.length == gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            },1000);
+        }
+    } else {
+        console.log('failed');
+    }
+    
+}
+
 //generate a new random number between 0 and 3,
 function nextSequence() {
+
+    // reset the userClickedPattern to an empty array ready for the next level.
+    userClickedPattern = [];
 
     //increase the level by 1 every time nextSequence() is called
     level++;
@@ -42,6 +61,7 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     // Add the new randomChosenColour to the end of the gamePattern.
     gamePattern.push(randomChosenColour);
+    // console.log(gamePattern);
 
     //select the button with the same id as the randomChosenColour and animate a flash 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
